ekuiter/linux

commit d7e41b631703b1c0f2c8c3c2a90c4e6aeacaea52

ovl: create directories inside merged parent opaque
The benefit of making directories opaque on creation is that lookups can
stop short when they reach the original created directory, instead of
continue lookup the entire depth of parent directory stack.

The best case is overlay with N layers, performing lookup for first level
directory, which exists only in upper.  In that case, there will be only
one lookup instead of N.

Signed-off-by: Amir Goldstein <amir73il@gmail.com>
Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

fs/overlayfs/dir.c
@@ -184,6 +184,11 @@ static void ovl_instantiate(struct dentry *dentry, struct inode *inode,
 	d_instantiate(dentry, inode);
 }
 
+static bool ovl_type_merge(struct dentry *dentry)
+{
+	return OVL_TYPE_MERGE(ovl_path_type(dentry));
+}
+
 static int ovl_create_upper(struct dentry *dentry, struct inode *inode,
 			    struct kstat *stat, const char *link,
 			    struct dentry *hardlink)
@@ -205,6 +210,11 @@ static int ovl_create_upper(struct dentry *dentry, struct inode *inode,
 	err = ovl_create_real(udir, newdentry, stat, link, hardlink, false);
 	if (err)
 		goto out_dput;
+
+	if (ovl_type_merge(dentry->d_parent)) {
+		/* Setting opaque here is just an optimization, allow to fail */
+		ovl_set_opaque(dentry, newdentry);
+	}
 
 	ovl_instantiate(dentry, inode, newdentry, !!hardlink);
 	newdentry = NULL;
@@ -1006,7 +1016,7 @@ static int ovl_rename(struct inode *olddir, struct dentry *old,
 	if (is_dir) {
 		if (ovl_type_merge_or_lower(old))
 			err = ovl_set_redirect(old, samedir);
-		else if (!old_opaque && ovl_lower_positive(new))
+		else if (!old_opaque && ovl_type_merge(new->d_parent))
 			err = ovl_set_opaque(old, olddentry);
 		if (err)
 			goto out_dput;
@@ -1014,7 +1024,7 @@ static int ovl_rename(struct inode *olddir, struct dentry *old,
 	if (!overwrite && new_is_dir) {
 		if (ovl_type_merge_or_lower(new))
 			err = ovl_set_redirect(new, samedir);
-		else if (!new_opaque && ovl_lower_positive(old))
+		else if (!new_opaque && ovl_type_merge(old->d_parent))
 			err = ovl_set_opaque(new, newdentry);
 		if (err)
 			goto out_dput;

commit 4aa0eaf374fffbead8a938752f6aa3dba60fb959

ovl: do not set overlay.opaque on non-dir create
The optimization for opaque dir create was wrongly being applied
also to non-dir create.

Fixes: d7e41b631703 ("ovl: create directories inside merged parent opaque")
Signed-off-by: Amir Goldstein <amir73il@gmail.com>
Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
Cc: <stable@vger.kernel.org> # v4.10

fs/overlayfs/dir.c
@@ -210,7 +210,7 @@ static int ovl_create_upper(struct dentry *dentry, struct inode *inode,
 	if (err)
 		goto out_dput;
 
-	if (ovl_type_merge(dentry->d_parent)) {
+	if (ovl_type_merge(dentry->d_parent) && d_is_dir(newdentry)) {
 		/* Setting opaque here is just an optimization, allow to fail */
 		ovl_set_opaque(dentry, newdentry);
 	}