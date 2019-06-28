ekuiter/linux

commit 9264e2da261a6b6cadcb677b24b36a8ff2cd532c

ASoC: samsung: arndale_rt5631: don't select unnecessary Platform
ALSA SoC is now supporting "no Platform". Sound card doesn't need to
select "CPU component" as "Platform" anymore if it doesn't need
special Platform.
This patch removes such settings.

Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
Signed-off-by: Mark Brown <broonie@kernel.org>

sound/soc/samsung/arndale_rt5631.c
@@ -52,8 +52,7 @@ static struct snd_soc_ops arndale_ops = {
 
 SND_SOC_DAILINK_DEFS(rt5631_hifi,
 	DAILINK_COMP_ARRAY(COMP_EMPTY()),
-	DAILINK_COMP_ARRAY(COMP_CODEC(NULL, "rt5631-hifi")),
-	DAILINK_COMP_ARRAY(COMP_EMPTY()));
+	DAILINK_COMP_ARRAY(COMP_CODEC(NULL, "rt5631-hifi")));
 
 static struct snd_soc_dai_link arndale_rt5631_dai[] = {
 	{
@@ -93,9 +92,6 @@ static int arndale_audio_probe(struct platform_device *pdev)
 				return -EINVAL;
 			}
 		}
-		if (!arndale_rt5631_dai[n].platforms->name)
-			arndale_rt5631_dai[n].platforms->of_node =
-					arndale_rt5631_dai[n].cpus->of_node;
 
 		arndale_rt5631_dai[n].codecs->name = NULL;
 		arndale_rt5631_dai[n].codecs->of_node = of_parse_phandle(np,

commit b52d851ce767028b1e0ad63c766dee54c75a9217

ASoC: samsung: arndale_rt5631: consider CPU-Platform possibility
commit 9264e2da261a ("ASoC: samsung: arndale_rt5631: don't select
unnecessary Platform")

Current ALSA SoC avoid to add duplicate component to rtd,
and this driver was selecting CPU component as Platform component.
Thus, above patch removed Platform settings from this driver,
because it assumed these are same component.

But, some CPU driver is using generic DMAEngine, in such case, both
CPU component and Platform component will have same of_node/name.
In other words, there are some components which are different but
have same of_node/name.

In such case, Card driver definitely need to select Platform even
though it is same as CPU.
It is depends on CPU driver, but is difficult to know it from Card driver.
This patch reverts above patch.

Fixes: commit 9264e2da261a ("ASoC: samsung: arndale_rt5631: don't select unnecessary Platform")
Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
Signed-off-by: Mark Brown <broonie@kernel.org>

sound/soc/samsung/arndale_rt5631.c
@@ -52,7 +52,8 @@ static struct snd_soc_ops arndale_ops = {
 
 SND_SOC_DAILINK_DEFS(rt5631_hifi,
 	DAILINK_COMP_ARRAY(COMP_EMPTY()),
-	DAILINK_COMP_ARRAY(COMP_CODEC(NULL, "rt5631-hifi")));
+	DAILINK_COMP_ARRAY(COMP_CODEC(NULL, "rt5631-hifi")),
+	DAILINK_COMP_ARRAY(COMP_EMPTY()));
 
 static struct snd_soc_dai_link arndale_rt5631_dai[] = {
 	{
@@ -92,6 +93,9 @@ static int arndale_audio_probe(struct platform_device *pdev)
 				return -EINVAL;
 			}
 		}
+		if (!arndale_rt5631_dai[n].platforms->name)
+			arndale_rt5631_dai[n].platforms->of_node =
+					arndale_rt5631_dai[n].cpus->of_node;
 
 		arndale_rt5631_dai[n].codecs->name = NULL;
 		arndale_rt5631_dai[n].codecs->of_node = of_parse_phandle(np,